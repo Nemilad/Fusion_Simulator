3c561492e182515b826f84daf06b8bc59dc0a612
Nemilad <Nik155@list.ru>
2020-04-06 11:20:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fusion_Simulator
+ 
---
+++ app.py
@@ -0,0 +1,159 @@
+import copy
+import sys
+import Arch_Settings
+from PyQt5 import QtWidgets
+from PyQt5 import uic
+
+User_settings = copy.deepcopy(Arch_Settings.Template_settings)
+
+Arch_Dict = {
+    'Pentium M': Arch_Settings.Pentium_M_settings,
+    'Core 2': Arch_Settings.Core_2_settings,
+    'Nehalem': Arch_Settings.Nehalem_settings,
+    'Sandy Bridge': Arch_Settings.Sandy_Bridge_settings,
+    'Ivy Bridge': Arch_Settings.Ivy_Bridge_settings,
+    'Haswell': Arch_Settings.Haswell_settings,
+    'Broadwell': Arch_Settings.Broadwell_settings,
+    'Skylake': Arch_Settings.Skylake_settings
+}
+
+
+class Ui(QtWidgets.QMainWindow):
+    current_radio = 'none'
+
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi("mainwindow.ui", self)
+        self.comboBox_arch.currentTextChanged.connect(self.arch_change)
+        for radio in self.scrollArea_macro_first_pair.findChildren(QtWidgets.QRadioButton):
+            radio.toggled.connect(self.show_second_operators)
+        for box in self.scrollArea_macro_second_pair.findChildren(QtWidgets.QCheckBox):
+            box.toggled.connect(self.second_operator_change_state)
+        self.checkBox_arch_macro.stateChanged.connect(self.macro_fusion_change_state)
+        self.checkBox_arch_micro.stateChanged.connect(self.micro_fusion_change_state)
+        for box in self.scrollArea_macro_operands.findChildren(QtWidgets.QCheckBox):
+            box.stateChanged.connect(self.macro_operands_change_state)
+        for box in self.scrollArea_macro_conditions.findChildren(QtWidgets.QCheckBox):
+            box.stateChanged.connect(self.macro_conditions_change_state)
+        for box in self.scrollArea_micro_pairs.findChildren(QtWidgets.QCheckBox):
+            box.stateChanged.connect(self.micro_pairs_change_state)
+        for box in self.scrollArea_micro_conditions.findChildren(QtWidgets.QCheckBox):
+            box.stateChanged.connect(self.micro_conditions_change_state)
+        self.pushButton_simulate.clicked.connect(self.simulate)
+        self.set_enabled_second_pair(0)
+        self.show()
+
+    def simulate(self):
+        print(User_settings)
+
+    def arch_change(self):
+        sender = self.sender()
+        last_item = sender.property('last_item')
+        if sender.currentText() != 'Своя':
+            current_dict = Arch_Dict[sender.currentText()]
+            for box in self.scrollArea_macro_conditions.findChildren(QtWidgets.QCheckBox):
+                box.setChecked(current_dict['Macro_Conditions'][box.objectName()[9:]])
+            for box in self.scrollArea_micro_pairs.findChildren(QtWidgets.QCheckBox):
+                box.setChecked(current_dict['Micro_Pairs'][box.objectName()[9:]])
+            for box in self.scrollArea_micro_conditions.findChildren(QtWidgets.QCheckBox):
+                box.setChecked(current_dict['Micro_Conditions'][box.objectName()[15:]])
+            for box in self.scrollArea_macro_operands.findChildren(QtWidgets.QCheckBox):
+                box.setChecked(current_dict['Macro_Operands'][box.objectName()[9:]])
+            self.checkBox_arch_macro.setChecked(current_dict['Macro_Micro']['Macro'])
+            self.checkBox_arch_micro.setChecked(current_dict['Macro_Micro']['Micro'])
+            self.set_enabled_all(0)
+        else:
+            self.set_enabled_all(1)
+            if last_item != 'Своя' or 'None':
+                global User_settings
+                User_settings = copy.deepcopy(Arch_Dict[last_item])
+        self.clear_second_operators()
+        sender.setProperty('last_item', sender.currentText())
+
+    def macro_fusion_change_state(self):
+        sender = self.sender()
+        global User_settings
+        if self.comboBox_arch.currentText() == 'Своя':
+            User_settings['Macro_Micro']['Macro'] = int(sender.isChecked())
+
+    def micro_fusion_change_state(self):
+        sender = self.sender()
+        global User_settings
+        if self.comboBox_arch.currentText() == 'Своя':
+            User_settings['Macro_Micro']['Micro'] = int(sender.isChecked())
+
+    def second_operator_change_state(self):
+        sender = self.sender()
+        global User_settings
+        if self.comboBox_arch.currentText() == 'Своя' and self.current_radio != 'none':
+            User_settings['Macro_Pairs'][self.current_radio][sender.objectName()[9:]] = int(sender.isChecked())
+
+    def macro_operands_change_state(self):
+        sender = self.sender()
+        global User_settings
+        if self.comboBox_arch.currentText() == 'Своя':
+            User_settings['Macro_Operands'][sender.objectName()[9:]] = int(sender.isChecked())
+
+    def macro_conditions_change_state(self):
+        sender = self.sender()
+        global User_settings
+        if self.comboBox_arch.currentText() == 'Своя':
+            User_settings['Macro_Conditions'][sender.objectName()[9:]] = int(sender.isChecked())
+
+    def micro_conditions_change_state(self):
+        sender = self.sender()
+        global User_settings
+        if self.comboBox_arch.currentText() == 'Своя':
+            User_settings['Micro_Conditions'][sender.objectName()[15:]] = int(sender.isChecked())
+
+    def micro_pairs_change_state(self):
+        sender = self.sender()
+        global User_settings
+        if self.comboBox_arch.currentText() == 'Своя':
+            User_settings['Micro_Pairs'][sender.objectName()[9:]] = int(sender.isChecked())
+
+    def show_second_operators(self):
+        sender = self.sender()
+        self.current_radio = sender.objectName()[12:]
+        if self.comboBox_arch.currentText() == 'Своя':
+            current_dict = User_settings
+            self.set_enabled_second_pair(1)
+        else:
+            current_dict = Arch_Dict[self.comboBox_arch.currentText()]
+        if sender.isChecked():
+            for box in self.scrollArea_macro_second_pair.findChildren(QtWidgets.QCheckBox):
+                box.setChecked(current_dict['Macro_Pairs'][sender.objectName()[12:]][box.objectName()[9:]])
+
+    def clear_second_operators(self):
+        for radio in self.scrollArea_macro_first_pair.findChildren(QtWidgets.QRadioButton):
+            if radio.isChecked():
+                radio.setAutoExclusive(0)
+                radio.setChecked(0)
+                radio.setAutoExclusive(1)
+        for box in self.scrollArea_macro_second_pair.findChildren(QtWidgets.QCheckBox):
+            box.setChecked(0)
+        self.current_radio = 'none'
+        self.set_enabled_second_pair(0)
+
+    def set_enabled_all(self, state):
+        for box in self.scrollArea_macro_second_pair.findChildren(QtWidgets.QCheckBox):
+            box.setEnabled(state)
+        for box in self.scrollArea_macro_conditions.findChildren(QtWidgets.QCheckBox):
+            box.setEnabled(state)
+        for box in self.scrollArea_micro_pairs.findChildren(QtWidgets.QCheckBox):
+            box.setEnabled(state)
+        for box in self.scrollArea_macro_operands.findChildren(QtWidgets.QCheckBox):
+            box.setEnabled(state)
+        for box in self.scrollArea_micro_conditions.findChildren(QtWidgets.QCheckBox):
+            box.setEnabled(state)
+        self.checkBox_arch_macro.setEnabled(state)
+        self.checkBox_arch_micro.setEnabled(state)
+
+    def set_enabled_second_pair(self, state):
+        for box in self.scrollArea_macro_second_pair.findChildren(QtWidgets.QCheckBox):
+            box.setEnabled(state)
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+sys.exit(app.exec())
